e7435a85e30ca0a085b7f5fcaf514cccaf5fdc81
MengOonLee <darklemon2000@gmail.com>
2023-06-05 00:28:10 +0800

    html div

--- Plotly_Dash/app.py
+++ Plotly_Dash/app.py
@@ -1,38 +1,32 @@
 import numpy as np
 import dash
 import plotly.express as px
-import plotly.graph_objects as go
+
+df = px.data.gapminder()
+
+df_line = df.groupby(by=['continent', 'year'])\
+    .agg(avgGDPCap=('gdpPercap', np.average))\
+    .reset_index()
+fig_line = px.line(data_frame=df_line,
+    x='year', y='avgGDPCap', color='continent',
+    title='Avg GDP/Cap by Year')
+
+df_bar = df.groupby(by='continent')\
+    .agg(avgGDPCap=('gdpPercap', np.average))\
+    .reset_index()
+fig_bar = px.bar(data_frame=df_bar,
+    x='avgGDPCap', y='continent', color='continent',
+    title='Avg GDP/Cap by Continent', orientation='h')
+max_bar = df_bar.iloc[np.argmax(df_bar['avgGDPCap'])]
 
 app = dash.Dash(__name__)
-app.layout = dash.html.Div([
-    dash.dcc.Graph(
-        figure=go.Figure({
-            'data': [{
-                'type': 'bar',
-                'x': ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
-                    'Friday', 'Saturday', 'Sunday'],
-                'y': [28, 27, 25, 31, 32, 35, 36]
-            }],
-            'layout': {
-                'title': {
-                    'text': 'Temperatures of the week',
-                    'x': 0.5,
-                    'font': {
-                        'color': 'red',
-                        'size': 15
-                    }
-                }
-            }
-        })
-    ),
-    dash.dcc.Graph(
-        figure=px.bar(
-            data_frame=px.data.tips()\
-                .groupby(by='day').agg(avg_tip=('tip', np.average))\
-                .reset_index(),
-            x='day', y='avg_tip', color='avg_tip'
-        )
-    )
+app.layout = dash.html.Div(children=[
+    dash.html.H1('Avg GDP/Cap Dashboard'),
+    dash.html.Div(dash.dcc.Graph(id='fig_line', figure=fig_line)),
+    dash.html.Div(dash.dcc.Graph(id='fig_bar', figure=fig_bar)),
+    dash.html.H3(f"The largest continent by avg GDP/Cap is \
+        {max_bar['continent']}")
 ])
+
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', debug=True)
