a88c71b880b31457a4965889fe36fbb675f63d8d
MengOonLee <darklemon2000@gmail.com>
2023-06-08 00:16:55 +0800

    Dash: inline-block

--- Plotly_Dash/app.py
+++ Plotly_Dash/app.py
@@ -5,36 +5,45 @@ import plotly.express as px
 df = px.data.gapminder()
 logo_link = 'https://avatars.githubusercontent.com/u/44514389?v=4'
 
-df_bar = df.groupby(by=['continent', 'country'])\
+df_line = df.groupby(by=['continent', 'year'])\
     .agg(avgGDPCap=('gdpPercap', np.average))\
-    .sort_values(by='avgGDPCap', ascending=False)\
-    .reset_index()
-top_country = df_bar.loc[0]['country']
+    .reset_index()\
+    .sort_values(by=['continent', 'year'])
+fig_line = px.line(data_frame=df_line,
+    x='year', y='avgGDPCap', color='continent')
+fig_line.update_layout({'paper_bgcolor':'rgb(224, 255, 252)'})
+
+df_bar = df.groupby(by='continent')\
+    .agg(avgGDPCap=('gdpPercap', np.average))\
+    .reset_index()\
+    .sort_values(by='avgGDPCap', ascending=False)
 fig_bar = px.bar(data_frame=df_bar,
-    x='avgGDPCap', y='country', color='continent')
+    x='avgGDPCap', y='continent', color='continent',
+    orientation='h')
 fig_bar.update_layout({
     'yaxis':{'dtick':5, 'categoryorder':'total ascending'},
-    'paper_bgcolor':'rgb(224, 255, 252)'
-})
+    'paper_bgcolor':'rgb(224, 255, 252)'})
 
 app = dash.Dash(__name__)
 app.layout = dash.html.Div(children=[
-    dash.html.Img(src=logo_link,
-        style={'width':'50px', 'height':'50px'}),
-    dash.html.H1("Avg GDP/Cap by country"),
-    dash.html.Div(dash.dcc.Graph(figure=fig_bar,
-        style={'width':'500px', 'height':'500px', 'margin':'auto'})),
-    dash.html.Br(),
-    dash.html.Span(children=[
-        "The top country is: ",
-        dash.html.B(top_country),
-        dash.html.Br(),
-        dash.html.I('Copyright E-Com Inc',
-            style={'background-color':'lightgrey'})
-    ])
+    dash.html.Div(children=[
+        dash.html.Img(src=logo_link,
+            style={'width':'50px', 'height':'50px',
+                'display':'inline-block', 'margin':'25px'}),
+        dash.html.H1(children=['Avg GDP/Cap Dashboard'],
+            style={'display':'inline-block'}),
+        dash.html.Img(src=logo_link,
+            style={'width':'50px', 'height':'50px',
+                'display':'inline-block', 'margin':'25px'})]),
+    dash.html.Div(dash.dcc.Graph(figure=fig_line),
+        style={'width':'500px', 'display':'inline-block',
+            'margin':'5px'}),
+    dash.html.Div(dash.dcc.Graph(figure=fig_bar),
+        style={'width':'350px', 'display':'inline-block',
+            'margin':'5px'}),
+    dash.html.H3(f"The largest GDP/Cap was {df['gdpPercap'].max()}")
     ], style={'text-align':'center', 'font-size':22,
-        'background-color':'rgb(224, 255, 252)'}
-)
+        'background-color':'rgb(224, 255, 252)'})
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', debug=True)
