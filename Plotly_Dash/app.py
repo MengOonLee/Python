ba8bc80dbc31500d9958277ed34a9a900fabbff3
MengOonLee <darklemon2000@gmail.com>
2023-06-11 01:05:44 +0800

    Dash: choropleth

--- Plotly_Dash/app.py
+++ Plotly_Dash/app.py
@@ -2,7 +2,6 @@ import numpy as np
 from datetime import datetime
 import dash
 import plotly.express as px
-import plotly.graph_objects as go
 
 logo_link = 'https://avatars.githubusercontent.com/u/44514389?v=4'
 
@@ -12,28 +11,29 @@ country1, lifeExp1 = df.iloc[np.argmax(df['lifeExp'])]\
 country2, lifeExp2 = df.iloc[np.argmin(df['lifeExp'])]\
     [['country', 'lifeExp']]
 
-marker_life_gdp = go.Figure()
-for c in df['continent'].unique():
-    mask = df['continent']==c
-    marker_life_gdp.add_trace(go.Scatter(
-        x=df[mask]['gdpPercap'],
-        y=df[mask]['lifeExp'],
-        name=c,
-        text=df[mask]['country'],
-        mode='markers',
-        marker={'size':15, 'line':{'width':0.5, 'color':'white'}},
-        opacity=0.8
-    ))
+marker_life_gdp = px.scatter(data_frame=df,
+    x='gdpPercap', y='lifeExp', color='continent',
+    size='pop', size_max=50, log_x=True,
+    range_x=[np.min(df['gdpPercap']),
+        np.max(df['gdpPercap'])],
+    range_y=[np.min(df['lifeExp'])-10, np.max(df['lifeExp'])+10],
+    animation_frame='year', animation_group='country',
+    hover_name='country', labels={'gdpPercap':'GDP Per Capital',
+        'lifeExp':'Life Expectency', 'pop':'Population'}
+)
 marker_life_gdp.update_layout(
-    xaxis={'type':'log', 'title':'GDP per Capita'},
-    yaxis={'title':'Life Expectancy'},
-    legend={'x':0, 'y':1},
     hovermode='closest',
     margin={'l':40, 'b':40, 't':10, 'r':10},
-    paper_bgcolor='black',
-    font={'color':'white'}
+    template='plotly_dark'
 )
-    
+
+map_gdp = px.choropleth(data_frame=df, locations='iso_alpha',
+    color='gdpPercap', hover_name='country',
+    range_color=(np.min(df['gdpPercap']), np.max(df['gdpPercap'])),
+    animation_frame='year')
+map_gdp.update_layout(
+    template='plotly_dark')
+
 app = dash.Dash(__name__)
 app.layout = dash.html.Div(children=[
     dash.html.Img(src=logo_link,
@@ -50,11 +50,12 @@ app.layout = dash.html.Div(children=[
     dash.html.Img(src=logo_link,
         style={'width':'5%', 'height':'5%',
             'display':'inline-block', 'margin':'1%'}),
-    dash.html.H1("Continent Life Expectancy, GDP per Capita"),
+    dash.html.H1("Continent Life Expectency, GDP per Capital"),
     dash.html.Div(children=[
+        dash.dcc.Graph(id='map_gdp', figure=map_gdp),
         dash.dcc.Graph(id='marker_life_gdp', figure=marker_life_gdp),
         dash.html.Span(children=[
-            "The top & last country by life expectancy are:",
+            "The top & last country by life expectency are:",
             dash.html.Ol(children=[
                 dash.html.Li(children=[
                     country1, ", ", lifeExp1, " years"
@@ -64,7 +65,7 @@ app.layout = dash.html.Div(children=[
                 ])
             ], style={'width':'35%', 'margin':'auto'})
         ])
-    ], style={'width':'75%', 'margin':'auto'}),
+    ], style={'width':'80%', 'margin':'auto'}),
 ], style={'text-align':'center', 'font-size':18,
     'background-color':'black', 'color':'white'})
 
